86fbed8352cbb5caba14dbc485ccbfe31b8306b0
Nimo08 <nimomohamed800@gmail.com>
2023-12-02 09:35:03 +0300

    Added checks for place_id & amenity_id

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,9 +8,12 @@ from models.amenity import Amenity
 from api.v1.views import app_views
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
 def get_place_amenities(place_id):
     """Retrieve the list of all Amenity objects of a Place."""
+    if place_id is None:
+        abort(404)
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
@@ -24,9 +27,11 @@ def get_place_amenities(place_id):
     return jsonify(amenities)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_place_amenity(place_id, amenity_id):
+    if place_id is None or amenity_id is None:
+        abort(404)
     """Delete an Amenity object from a Place."""
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
@@ -52,6 +57,8 @@ def delete_place_amenity(place_id, amenity_id):
                  strict_slashes=False)
 def link_amenity_to_place(place_id, amenity_id):
     """Link an Amenity object to a Place."""
+    if place_id is None or amenity_id is None:
+        abort(404)
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
 
